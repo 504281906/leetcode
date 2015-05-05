a66c1814ade93279245ff29339acee5de49b3e1f
Modiz <504281906@qq.com>
2015-05-05 23:00:45 +0800

    Reverse Linked List
    单向链表反转

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,50 @@
+#include<limits>
+#include<queue>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL __int64
+#define eps 1e-8
+#define pi acos(-1)
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    	if (head==NULL) return NULL;
+        ListNode* ans=NULL;
+        while (head){
+			ListNode* next=head->next;
+			head->next=ans;
+			ans=head;
+			head=next;
+        }
+        return ans;
+    }
+};
+
+int main(){
+	return 0;
+}
