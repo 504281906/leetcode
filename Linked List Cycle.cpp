bc5efd1bffb00ced2c2a5bfb67ce20bf3f38f0e5
Modiz <504281906@qq.com>
2015-10-05 19:07:27 +0800

    判断链表是否成环
    上次再百度一面的时候也考了这个，但是时间缘故，没有写，但是已经有了想法和面试官说了下，这次看到了，写出来了。想法没错，以前不记得是否见到过。
    
    定义两个指针都指向头指针，然后第一个指针a每次走一步，第二个指针b每次走两步，看是否两个指针指向同一个位置，或者是b指针为NULL了。等于b在追a吧。（如果是有环的话）

---
+++ Linked List Cycle.cpp
@@ -0,0 +1,52 @@
+#include<limits>
+#include<queue>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL __int64
+#define eps 1e-8
+#define pi acos(-1)
+#define delta 0.98 //模拟退火递增变量
+#define INF 0x7fffffff
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *a=head;
+        ListNode *b=head;
+        if (b!=NULL && b->next!=NULL){
+        	a=a->next;
+        	b=b->next->next;
+        	if (a==b) return true;
+        }
+        return false;
+    }
+};
+int main(){
+	return 0;
+}
+
