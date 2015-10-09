16b553495eeebc1b4f5802a5f7378f54b95b4e65
Modiz <504281906@qq.com>
2015-10-09 20:59:21 +0800

    一遍AC大法好，求完全二叉树的next.
    一个个找。左儿子next肯定是右儿子，右儿子是否有next就看他爸比是否有next的；咯。蛤蛤

---
+++ Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,49 @@
+#include<limits>
+#include<queue>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL __int64
+#define eps 1e-8
+#define pi acos(-1)
+#define delta 0.98 //模拟退火递增变量
+#define INF 0x7fffffff
+using namespace std;
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+    	if (root==NULL) return;
+    	if (root->left==NULL) return;
+    	root->left->next=root->right;
+    	if (root->next!=NULL)
+    		root->right->next=root->next->left;
+    	connect(root->left);
+    	connect(root->right);
+    	return;
+    }
+};
+int main(){
+	return 0;
+}
