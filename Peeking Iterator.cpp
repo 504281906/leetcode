966e1dedd27a042dcbf12351d0b174807f6b48c5
Modiz <504281906@qq.com>
2015-11-08 20:14:57 +0800

    改迭代器的接口功能吧，
    增加一个peek功能，能够窥见下一个节点的值，但是不移动指针。用一个缓存和判断吧。

---
+++ Peeking Iterator.cpp
@@ -0,0 +1,85 @@
+#include<limits>
+#include<queue>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL __int64
+#define eps 1e-8
+#define pi acos(-1)
+#define delta 0.98 //模拟退火递增变量
+#define INF 0x7fffffff
+using namespace std;
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+	int k;
+	bool f;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        if (!f){
+        	f=1;
+        	return k=Iterator::next();
+        }else return k;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    if (f){
+	    	f=0;
+	    	return k; 
+	    }else{
+	    	return Iterator::next();
+	    }
+	}
+
+	bool hasNext() const {
+	    if (f){
+	    	return true;
+	    }else{
+	    	return Iterator::hasNext();
+	    }
+	}
+};
+int main(){
+	
+	return 0;
+}
